68cfff996dd6b00a9679e33e9e641538f56813e9
3pidemix <3pidemix@gmail.com>
2020-09-20 18:48:18 -0400

    Adds .gitignore, updates README and scraper.py for scraper

--- Schedule_Optimizer/scraper/README.md
+++ Schedule_Optimizer/scraper/README.md
@@ -1,13 +1,32 @@
 # scraper.py Instructions
-The `scraper.py` script will crawl a subject's course catalog and audit web page, parse them, and put the relevant data into the backend's MySQL database.
+The `scraper.py` script will crawl a subject's course catalog and audit web page, parse them, and put the relevant data into the backend's MySQL database. The scraper depends on:
+1. [requests](https://requests.readthedocs.io/en/master/), for connecting to and parsing websites
+2. [lxml](https://pypi.org/project/lxml/), which is used to extract data from HTML documents
+3. [SQLAlchemy](https://www.sqlalchemy.org), an Object Relational Mapping (ORM) package to convert from Python objects to database schemas
+4. [MySQL Python Connector](https://dev.mysql.com/doc/connector-python/en/), for connecting to the MySQL database 
 
 ```
-usage: scraper.py [-h] major password
+./scraper.py -h
+usage: scraper.py [-h] [-m MAJOR] [-i] password
 
 positional arguments:
-  major       The major you wish to scrape
-  password    Password for the MySQL database
+  password              Password for the MySQL database
 
 optional arguments:
-  -h, --help  show this help message and exit
+  -h, --help            show this help message and exit
+  -m MAJOR, --major MAJOR
+                        The major you wish to scrape
+  -i, --init            Initialize the database
 ```
+
+The scraper should be invoked by the backend application when:
+1. Initializing the application
+2. Requesting a new subject's courses and audit data to be added to the database
+
+The return code shall be 0 when the requested operation completed successfully, or non-zero if an error occurred.
+
+# MySQL Schema
+The database populated by the scraper contains two primary tables:
+1. courses -- this table contains the individual course data scraped from the course catalog
+2. audit -- this table contains the data scraped from the degree audit web pages
+
--- Schedule_Optimizer/scraper/scraper.py
+++ Schedule_Optimizer/scraper/scraper.py
@@ -8,60 +8,86 @@ from mysql.connector import connect, errorcode, Error
 import sqlalchemy
 import requests
 
-parser = argparse.ArgumentParser()
-parser.add_argument("major", help="The major you wish to scrape", type=str)
-parser.add_argument("password", help="Password for the MySQL database", type=str)
-args = parser.parse_args()
 
-"""
-Found search URL from looking at https://webappprd.acs.ncsu.edu/php/coursecat/directory.php 
-Using Chrome, use Cmd+Option+J to open the debug viewer, go to network tab, hit record.
-On page, select undergrad as career and CSC as subject, press the magnifying glass button.
-Then in the debug view, go to Network, and observe the directory_search.php element. In the
-Headers tab, look at the Form Data section to see what needs to be sent as data in the 
-requests.post() call.
-"""
-# Map major to data
-MAJORS = {"CSC": {
-    "search_val": 'CSC - COMPUTER SCIENCE',
-    "catalog_url": "https://webappprd.acs.ncsu.edu/php/coursecat/directory_search.php",
-    "audit_url": "https://oucc.dasa.ncsu.edu/coe-14cscbs-nosubplan-2177/"
+def initialize(password):
+    """ Initialize the database by creating the tables
+        Simply return without initializing if the tables already exist
+    """
+    if password:
+        print("Initializing database")
+    else:
+        print("Error: password required to initialize database")
+    return
+
+def get_courses():
+    return
+
+def get_audit():
+    return
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("password", help="Password for the MySQL database", type=str)
+    parser.add_argument("-m", "--major", help="The major you wish to scrape", type=str)
+    parser.add_argument('-i', '--init', help="Initialize the database", action="store_true")
+    args = parser.parse_args()
+
+    if args.init:
+        initialize(args.password)
+        return
+
+    """
+    Found search URL from looking at https://webappprd.acs.ncsu.edu/php/coursecat/directory.php 
+    Using Chrome, use Cmd+Option+J to open the debug viewer, go to network tab, hit record.
+    On page, select undergrad as career and CSC as subject, press the magnifying glass button.
+    Then in the debug view, go to Network, and observe the directory_search.php element. In the
+    Headers tab, look at the Form Data section to see what needs to be sent as data in the 
+    requests.post() call.
+    """
+    # Map major to data
+    MAJORS = {"CSC": {
+        "search_val": 'CSC - COMPUTER SCIENCE',
+        "catalog_url": "https://webappprd.acs.ncsu.edu/php/coursecat/directory_search.php",
+        "audit_url": "https://oucc.dasa.ncsu.edu/coe-14cscbs-nosubplan-2177/"
+        }
     }
-}
 
-major = args.major.upper()
-query_data = MAJORS.get(major)
+    major = args.major.upper()
+    query_data = MAJORS.get(major)
 
-if not query_data:
-    print(f'{major} major not supported')
-    exit(1)
+    if not query_data:
+        print(f'{major} major not supported')
+        exit(1)
 
-raw_query = requests.post(query_data["catalog_url"], data={'search_val': query_data["search_val"], 'type': 'subject', 'career': 'UGRD'})
-query_text = json.loads(raw_query.text)
-courses = query_text.get('courses')
+    raw_query = requests.post(query_data["catalog_url"], data={'search_val': query_data["search_val"], 'type': 'subject', 'career': 'UGRD'})
+    query_text = json.loads(raw_query.text)
+    courses = query_text.get('courses')
 
-raw_audit = requests.get(query_data["audit_url"])
-tree = html.fromstring(raw_audit.content)
-row_elements = tree.xpath('//tr')
+    raw_audit = requests.get(query_data["audit_url"])
+    tree = html.fromstring(raw_audit.content)
+    row_elements = tree.xpath('//tr')
 
-for e in row_elements:
-    # Need to parse subtree to get superscript
-    row_data = [col.text for col in e]
-    print(row_data)
+    for e in row_elements:
+        # Need to parse subtree to get superscript
+        row_data = [col.text for col in e]
+        print(row_data)
 
-# try:
-#     mydb = connect(host="localhost", user="root", password=args.password)
-# except Error as err:
-#     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-#         print("user/pass error")
-#     elif err.errno == errorcode.ER_BAD_DB_ERROR:
-#         print("Database error")
-#     else:
-#         print(err)
-# else:
-#     mydb.close()
+    # try:
+    #     mydb = connect(host="localhost", user="root", password=args.password)
+    # except Error as err:
+    #     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    #         print("user/pass error")
+    #     elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    #         print("Database error")
+    #     else:
+    #         print(err)
+    # else:
+    #     mydb.close()
 
-# print(mydb)
+    # print(mydb)
 
-sqleng = sqlalchemy.create_engine(f'mysql+mysqlconnector://root:{args.password}@localhost/recourse')
-print(sqleng)
+    sqleng = sqlalchemy.create_engine(f'mysql+mysqlconnector://root:{args.password}@localhost/recourse')
+    print(sqleng)
+
+if __name__ == "__main__":
+    main()
